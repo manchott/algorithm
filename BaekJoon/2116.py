f331024dda83ed752695189c0824f9a6685c6a5a
manchott <manchott@naver.com>
2022-07-19 17:59:38 +0900

    2116_in_progress

--- BaekJoon/2116.py
+++ BaekJoon/2116.py
@@ -12,36 +12,44 @@ max_height = max(s[1] for s in storage)     # 가장 높은 기둥
 result = 0
 fmh = 0
 lmh = 0
-print(storage)
-for i in range(1, n + 1):
-    print("i: ", i)
+pnum, pheight = storage[0][0], storage[0][1]
+for i in range(1, n):
+    if pheight == max_height:
+        break
+    print(i)
     cnum, cheight = storage[i][0], storage[i][1]
-    pnum, pheight = storage[i - 1][0], storage[i - 1][1]
-    print("c num, height", cnum, cheight)
     if cheight <= max_height:
         if cheight < pheight:  # 이전것보다 높이가 낮다면 제외
             continue
         else:
             result += (cnum - pnum) * pheight
-            print('rr: ', result)
+            pnum, pheight = cnum, cheight
     if cheight == max_height:
         fmh = i
         break
 
+pnum, pheight = storage[-1][0], storage[-1][1]
 for i in range(n - 2, 0, -1):
-    print("i: ", i)
+    print("i:", i)
     cnum, cheight = storage[i][0], storage[i][1]
-    pnum, pheight = storage[i + 1][0], storage[i + 1][1]
     if cheight <= max_height:
         if cheight < pheight:  # 이전것보다 높이가 낮다면 제외
             continue
         else:
+            print("else")
             result += (pnum - cnum) * pheight
+            pnum, pheight = cnum, cheight
     if cheight == max_height:
         lmh = i
         break
-
-    print(0)
 result += (lmh - fmh + 1) * max_height
 
 print(result)
+
+'''
+4
+8 10
+11 4
+13 6
+15 8
+'''
