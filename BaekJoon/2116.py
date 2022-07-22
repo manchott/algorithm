31401e32e2e918d9ba7d72bbfd80a63527287a17
manchott <manchott@naver.com>
2022-07-22 09:55:32 +0900

    2116 complete

--- BaekJoon/2116.py
+++ BaekJoon/2116.py
@@ -3,53 +3,39 @@ storage = []
 
 for _ in range(n):
     storage.append(list(map(int, input().split())))
-storage.sort()
-start = storage[0][1]
-end = storage[-1][1]
-max_height = max(s[1] for s in storage)     # 가장 높은 기둥
 
-# print(start, end, max_height)
-result = 0
-fmh = 0
-lmh = 0
-pnum, pheight = storage[0][0], storage[0][1]
-for i in range(1, n):
-    if pheight == max_height:
-        break
-    print(i)
-    cnum, cheight = storage[i][0], storage[i][1]
-    if cheight <= max_height:
-        if cheight < pheight:  # 이전것보다 높이가 낮다면 제외
-            continue
-        else:
-            result += (cnum - pnum) * pheight
-            pnum, pheight = cnum, cheight
-    if cheight == max_height:
-        fmh = i
-        break
+# 위치 순으로 정렬
+storage.sort(key=lambda x: x[0])
 
-pnum, pheight = storage[-1][0], storage[-1][1]
-for i in range(n - 2, 0, -1):
-    print("i:", i)
-    cnum, cheight = storage[i][0], storage[i][1]
-    if cheight <= max_height:
-        if cheight < pheight:  # 이전것보다 높이가 낮다면 제외
-            continue
-        else:
-            print("else")
-            result += (pnum - cnum) * pheight
-            pnum, pheight = cnum, cheight
-    if cheight == max_height:
-        lmh = i
-        break
-result += (lmh - fmh + 1) * max_height
+# 가장 높은 기둥의 높이
+max_idx = storage.index(max(storage, key=lambda x: x[1]))
+max_height = storage[max_idx][1]
 
-print(result)
+# 최대 넓이
+storage_area = max_height * (storage[-1][0] - storage[0][0] + 1)
 
-'''
-4
-8 10
-11 4
-13 6
-15 8
-'''
+# 앞에서부터 max_height 기둥까지 반복하며 필요 없는 부분 뺴기
+idx = storage[0][0]
+height = storage[0][1]
+for i in range(1, max_idx + 1):
+    if storage[i][1] > height:
+        storage_area -= (storage[i][0] - storage[0][0]
+                         ) * (storage[i][1] - height)
+        idx = storage[i][0]
+        height = storage[i][1]
+    else:
+        continue
+
+# 뒤에서부터 max_height 기둥까지 반복하며 필요 없는 부분 뺴기
+idx = storage[-1][0]
+height = storage[-1][1]
+for i in range(n - 2, max_idx - 1, -1):
+    if storage[i][1] > height:
+        storage_area -= (storage[-1][0] - storage[i]
+                         [0]) * (storage[i][1] - height)
+        idx = storage[i][0]
+        height = storage[i][1]
+    else:
+        continue
+
+print(storage_area)
