c46244eeedfd2e1ab83af6825709e047bf253f97
manchott <manchott@naver.com>
2022-07-26 09:34:19 +0900

    2477_complete

--- BaekJoon/2477.py
+++ BaekJoon/2477.py
@@ -1,32 +1,16 @@
-n = int(input())
+melon = int(input())
 side1 = []
 side2 = []
 side = []
 for i in range(6):
     x, y = map(int, input().split())
     side.append(y)
+side1_max = max(side[::2])
+side2_max = max(side[1::2])
+side1_max_idx = side.index(side1_max)
+side2_max_idx = side.index(side2_max)
 
-if i % 2:
-    side1.append(y)
-else:
-    side2.append(y)
-
-t = max(side1)
-j = max(side2)
-x, y = side1.index(t), side2.index(j)
-side1.pop(y)
-side2.pop(x)
-print(t, j, x, y)
-
-
-a = abs(side1[1] - side1[0])
-b = abs(side2[1] - side2[0])
-
-print(t, j, x, y, a, b)
-result = (t * j) - (a * b)
-
-print(result * n)
-# 1 동
-# 2 서
-# 3 남
-# 4 북
+little_side1 = abs(side[(side1_max_idx + 1) % 6] - side[side1_max_idx - 1])
+little_side2 = abs(side[(side2_max_idx + 1) % 6] - side[side2_max_idx - 1])
+result = (side1_max * side2_max) - (little_side1 * little_side2)
+print(result * melon)
