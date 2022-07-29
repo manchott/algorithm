33f477cbdf77d4a4bafd3a58be7b30dee93d785c
manchott <manchott@naver.com>
2022-07-29 16:15:39 +0900

    2447_complete

---
+++ BaekJoon/2447.py
@@ -0,0 +1,24 @@
+
+
+def draw_star(n):
+    if n == 1:
+        return ['*']
+
+    Stars = draw_star(n // 3)
+    L = []
+
+    for star in Stars:
+        print(star)
+        L.append(star*3)
+    for star in Stars:
+        print(star)
+        L.append(star+' '*(n//3)+star)
+    for star in Stars:
+        print(star)
+        L.append(star*3)
+
+    return L
+
+
+N = int(input())
+print('\n'.join(draw_star(N)))
