2c1c6a8313e1322a89267648a0c3f6622c9e4308
manchott <manchott@naver.com>
2022-09-15 22:41:57 +0900

    0915

--- BaekJoon/1175 - 숫자놀이.py
+++ BaekJoon/1175 - 숫자놀이.py
@@ -1,12 +1,19 @@
 M, N = map(int, input().split())
-alpha = ['zero', 'one', 'two', 'three', 'four',
-         'five', 'six', 'seven', 'eight', 'nine']
-result = []
+alpha = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+sort_alpha = []
 for i in range(M, N + 1):
+    num = str(i)
     temp = []
-    while i > 0:
-        num = i // 10
-        temp.append(alpha[num])
-        i %= 10
-    result.append(' '.join(temp))
-print(*sorted(result))
+    for j in num:
+        temp.append(alpha[int(j)])
+    sort_alpha.append(temp)
+result = []
+for i in sorted(sort_alpha):
+    temp = ''
+    for j in i:
+        temp += str(alpha.index(j))
+    result.append(temp)
+idx = 0
+while idx < len(result):
+    print(' '.join(result[idx:idx+10]))
+    idx += 10
