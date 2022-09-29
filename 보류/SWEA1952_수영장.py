2b0cf3e2445f8d36695da0d53ab94c801d207860
Seoyoung Park <63185396+manchott@users.noreply.github.com>
2022-09-29 22:53:08 +0900

    add

--- 보류/SWEA1952_수영장.py
+++
@@ -1,7 +0,0 @@
-T = int(input())
-for tc in range(1, T + 1):
-    day, month, three, year = map(int, input())
-    plan = [0, 0] + list(map(int, input())) + [0, 0]
-    result = [0, 0, plan[2]]
-    for i in range(3, 15):
-        pass
